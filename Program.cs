fd36f98f2469f5d527d07ae935c221bfbf84cc98
zajac-mc <36758466+zajac-mc@users.noreply.github.com>
2021-07-07 20:21:27 +0200

    adding getPair() function

--- Program.cs
+++ Program.cs
@@ -7,14 +7,33 @@
 */
 
 using System;
+using System.IO;
+
 
 namespace Simple_Hangman
 {
     class Program
-    {
+    {   
         static void Main(string[] args)
         {
+            string listName = "countries_and_capitals.txt";
+            Random rnd = new Random();
+
+
+            Console.WriteLine(getPair());
             Console.WriteLine("Hello World!");
+
+            string getPair()
+        {
+            // TODO: Better error handling
+            if (!File.Exists(listName))
+            {
+                Console.WriteLine("Error");
+                return "ERROR";
+            }
+            Console.WriteLine("Para");
+            return "Para";
         }
-    }
+        }        
+    }    
 }
