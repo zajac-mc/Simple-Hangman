95afe014e8b4afa8740aa492b8dbbc2097c04260
zajac-mc <36758466+zajac-mc@users.noreply.github.com>
2021-07-07 19:37:46 +0200

    First files

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿/*  
+    2021 07 07 Marcin Zajac
+    The Hangman Game Implemmentation
+
+    Implemenentation of the simple hangman according to functional requirements (more in .pdf attachement)
+    and my best knowledge
+*/
+
+using System;
+
+namespace Simple_Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
